f73bf6505d420ebdee6f4ffb5da6b026ad163a4e
Nayana Hettiarachchi <nayana@corp-gems.com>
2017-03-13 13:25:12 +0700

    initial working version

--- src/scala/org/gnuzero/BitcoinProxy.scala
+++ src/scala/org/gnuzero/BitcoinProxy.scala
@@ -1,24 +1,63 @@
 package org.gnuzero
 
+import org.gnuzero.bitcoin._
 import akka.http.scaladsl.server.Directives
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import spray.json._
+import scala.concurrent.Future
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.server.Directives._
+import akka.stream.ActorMaterializer
+import scala.io.StdIn
 
 // domain model
-final case class Wallet(name: String, id: Long)
-
 // collect your json format instances into a support trait:
 trait JsonSupport extends SprayJsonSupport with DefaultJsonProtocol {
-  implicit val itemFormat = jsonFormat2(Item)
-  implicit val orderFormat = jsonFormat1(Order) // contains List[Item]
+  implicit val itemFormat = jsonFormat2(Wallet)
 }
+
+case class Wallet(name: String, id: Long)
+
+
 
 // use it wherever json (un)marshalling is needed
-class BitcoinProxy extends Directives with JsonSupport {
-  // format: OFF
-  val route =
-      post {
+object BitcoinProxy extends Directives with JsonSupport {
 
+  implicit val system = ActorSystem("my-system")
+  implicit val materializer = ActorMaterializer()
+  // needed for the future flatMap/onComplete in the end
+  implicit val executionContext = system.dispatcher
+
+
+  def main(args: Array[String]): Unit = {
+
+
+
+    val route =
+      path("wallet" / "create") {
+        post {
+          complete(RpcService.createWallet())
+        }
       }
-  // format: ON
+
+    val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
+  }
+
+  object RpcService {
+
+    def createWallet():Future[Wallet] = {
+      implicit val jsonrpc = new JSONRPC("http://54.235.237.34:49568", "coind", "dnioc")
+      val addr = BitcoinRPC.getnewaddressForAccount("fooo") match {
+        case Right(y:String) => y
+      }
+      Future {
+        Wallet(name = addr, id = 123L)
+      }
+    }
+  }
+
 }
+
+
